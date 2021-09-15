4188ccdadee403f09104af0fb5f6d782e8141a49
David Stone <daystone4@gmail.com>
2021-09-15 14:23:52 -0400

    initial commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'david-portfolio',
+    title: 'David Stone',
     htmlAttrs: {
       lang: 'en'
     },
